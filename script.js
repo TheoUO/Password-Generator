edaa61833d24006a599058fd8d885c330e81667d
TCodes <theodoreuokeke@gmail.com>
2023-07-01 00:35:19 +0100

    added assignments to JS file

--- script.js
+++ script.js
@@ -4,6 +4,24 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var numeric = '0123456789'
+var specialCharacter = '!@#$%^&*()-_=+`~[]{}/?\|'
+
+function generatePassword() {
+  // 1. Prompt the user for the password criteria
+  
+   
+  //  a. Password length between 8 and 128
+  //  b. Prompt Lowercase, uppercase, numbers or special characters
+  // 2. Validate the input
+  // 3. Generate password based on criteria
+  // 4. Display password to the page
+
+  return 'Generated password will go here!';
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
